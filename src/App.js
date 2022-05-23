a8805c30d1a1e3f7a20e6c3d393028ce62c13b99
jordles113 <jordan.t.romero@gmail.com>
2022-05-23 11:46:00 -0500

    testing staging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing ci/cd pipeline
+          testing staging branch
         </p>
         <a
           className="App-link"
